3cad4a58453a50be35d4fdc48c787986dc2ac664
kpuipath <kurtpetrek@gmail.com>
2019-01-03 18:11:42 -0500

    Updates

---
+++ precache-manifest.c41eaf63b954d3ab627815d72aeaaea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a05313b0d125ce826fee",
+    "url": "/hall-of-robots/static/css/main.144c1f3a.chunk.css"
+  },
+  {
+    "revision": "a05313b0d125ce826fee",
+    "url": "/hall-of-robots/static/js/main.a05313b0.chunk.js"
+  },
+  {
+    "revision": "a8cdd96952cdef5d8d3c",
+    "url": "/hall-of-robots/static/js/1.a8cdd969.chunk.js"
+  },
+  {
+    "revision": "5ed2c25a83c15fea8efd",
+    "url": "/hall-of-robots/static/js/runtime~main.5ed2c25a.js"
+  },
+  {
+    "revision": "df63503a622e402005c613968d72a290",
+    "url": "/hall-of-robots/index.html"
+  }
+];
